c552b1722966e17f76ce8bd19b336b3daf437546
Guru Sreevanshu Yerragolam <vanshu6991@gmail.com>
2026-01-17 20:16:33 +0100

    Attempt #3
    First working version for single plarticle tested with Stokes/Schiller-Naumann with and without slip correction

--- src/ParticleSolver/ParticleMarchSubstep.F90
+++ src/ParticleSolver/ParticleMarchSubstep.F90
@@ -46,25 +46,25 @@ subroutine ParticleMarchSubstep
         call CheckIsParticleLocal(lpp_list(np),lbound)
         if (lbound) then
             !!! Update particle
-            ! call UpdateParticleGridIndices(lpp_list(np))                        ! Update cell-indices of the particles at their new positions
+            call UpdateParticleGridIndices(lpp_list(np))                        ! Update cell-indices of the particles at their new positions
             call InitParticleAcceleration(lpp_list(np))                         ! Initialize particle acceleration
             call AddParticleAccelerationDrag(lpp_list(np),nfrc)                 ! Add fluid drag forces to particle acceleration   
-            ! call AddParticleAccelerationGravity(lpp_list(np))                   ! Add particle acceleration due to particle apparent weight   
+            call AddParticleAccelerationGravity(lpp_list(np))                   ! Add particle acceleration due to particle apparent weight   
             call UpdateParticleVelocity(lpp_list(np))                           ! Update velocity of particles using 3rd order Runge-Kutta time-stepping scheme
-            ! call UpdateParticlePosition(lpp_list(np))                           ! Update position of particles using (2nd order) Crank-Nicolson time-stepping scheme
-            ! call UpdateParticleLifeTime(lpp_list(np))                           ! Update life time of particles (time since injection)
-            ! call CalculateParticleSubStepTime(lpp_list(np),dlft)                ! Calculate time spent by particle in the domain during the substep
-            ! call CheckIsParticleGlobal(lpp_list(np),gbound)                     ! Check if particle is going to exit the simulation domain
-            ! if (.not.gbound) then
-            !     call CalculateParticleExit(lpp_list(np),pex_list(nq))           ! Calculate the exit primarily to know the exit time. This is fine because pex_list is always initialized with size(1)
-            !     call UpdateParticleSubStepTime(lpp_list(np),pex_list(nq),dlft)  ! Update time spent by particle in the domain during the substep
-            !     if ((pex_save).and.(time.ge.pex_ssta)) then
-            !         nq = nq + 1                                                 ! Update index only if the exit events are to be written, otherwise overwrite the first index
-            !         pex_actv = pex_actv + 1                                     ! Update total number of exit events are to be written
-            !         lpp_exit = lpp_exit + 1                                     ! Update count of exit events
-            !     end if
-            ! end if
-            ! call ApplyParticleDragForce(lpp_list(np),nfrc,dlft)                 ! Apply the drag force from the particle on the Eulerian field
+            call UpdateParticlePosition(lpp_list(np))                           ! Update position of particles using (2nd order) Crank-Nicolson time-stepping scheme
+            call UpdateParticleLifeTime(lpp_list(np))                           ! Update life time of particles (time since injection)
+            call CalculateParticleSubStepTime(lpp_list(np),dlft)                ! Calculate time spent by particle in the domain during the substep
+            call CheckIsParticleGlobal(lpp_list(np),gbound)                     ! Check if particle is going to exit the simulation domain
+            if (.not.gbound) then
+                call CalculateParticleExit(lpp_list(np),pex_list(nq))           ! Calculate the exit primarily to know the exit time. This is fine because pex_list is always initialized with size(1)
+                call UpdateParticleSubStepTime(lpp_list(np),pex_list(nq),dlft)  ! Update time spent by particle in the domain during the substep
+                if ((pex_save).and.(time.ge.pex_ssta)) then
+                    nq = nq + 1                                                 ! Update index only if the exit events are to be written, otherwise overwrite the first index
+                    pex_actv = pex_actv + 1                                     ! Update total number of exit events are to be written
+                    lpp_exit = lpp_exit + 1                                     ! Update count of exit events
+                end if
+            end if
+            call ApplyParticleDragForce(lpp_list(np),nfrc,dlft)                 ! Apply the drag force from the particle on the Eulerian field
             !!! Proceed to the next particle
             np = np + 1
         else
--- src/ParticleSolver/ParticleRoutines.F90
+++ src/ParticleSolver/ParticleRoutines.F90
@@ -160,42 +160,38 @@ subroutine AddParticleAccelerationDrag(p,f)
     real                                :: pvol,pcfd
 
     ! Compute interpolation coefficients
-    ! call CalcTrilinearInterpolationCoefficients(p,cffm,cffc)
+    call CalcTrilinearInterpolationCoefficients(p,cffm,cffc)
 
     ! Compute slip velocity
     !! Initialize slip velocity to zero
     slip(:) = 0.0
     !! Apply trilinear interpolation with previously calculated coefficients to get fluid velocity    
-    ! call ApplyTrilinearInterpolation(p,cffm,cffc,'x',1,vx,slip(1))
-    ! call ApplyTrilinearInterpolation(p,cffm,cffc,'y',1,vy,slip(2))
-    ! call ApplyTrilinearInterpolation(p,cffm,cffc,'z',1,vz,slip(3))
-
-    ! !! If slip correction is enabled and particle isn't just spawned
-    ! !! Correct the slip velocity to account for self-induced velocity
-    ! if (lpp_scor.and.(p%lpp_lft.gt.0)) then
-        
-    !     !!! Initialize curvatures to zero
-    !     curv(:) = 0.0
-    !     !!! Compute the curvature at the particle using trilinear interpolation
-    !     call ApplyTrilinearInterpolation(p,cffm,cffc,'x',1,lpp_d2vx,curv(1))
-    !     call ApplyTrilinearInterpolation(p,cffm,cffc,'y',1,lpp_d2vy,curv(2))
-    !     call ApplyTrilinearInterpolation(p,cffm,cffc,'z',1,lpp_d2vz,curv(3))
-    !     !!! Compute the correction coefficient using trilinear interpolation on lookup data array 
-    !     !!! Note: Reynolds number calculated at previous substep is being used to avoid complicated implicit calculation
-    !     call CalcSlipCorrectionCoefficient(p,'x',coef(1))
-    !     call CalcSlipCorrectionCoefficient(p,'y',coef(2))
-    !     call CalcSlipCorrectionCoefficient(p,'z',coef(3))
-    !     !!! Compute the slip correction using coefficients and curvatures
-    !     slip(1) = slip(1) + coef(1)*curv(1)
-    !     slip(2) = slip(2) + coef(2)*curv(2)
-    !     slip(3) = slip(3) + coef(3)*curv(3)
-
-    ! end if
-
+    call ApplyTrilinearInterpolation(p,cffm,cffc,'x',1,vx,slip(1))
+    call ApplyTrilinearInterpolation(p,cffm,cffc,'y',1,vy,slip(2))
+    call ApplyTrilinearInterpolation(p,cffm,cffc,'z',1,vz,slip(3))
+    !! If slip correction is enabled and particle isn't just spawned
+    !! Correct the slip velocity to account for self-induced velocity
+    if (lpp_scor.and.(p%lpp_lft.gt.0)) then
+        !!! Initialize curvatures to zero
+        curv(:) = 0.0
+        !!! Compute the curvature at the particle using trilinear interpolation
+        call ApplyTrilinearInterpolation(p,cffm,cffc,'x',1,lpp_d2vx,curv(1))
+        call ApplyTrilinearInterpolation(p,cffm,cffc,'y',1,lpp_d2vy,curv(2))
+        call ApplyTrilinearInterpolation(p,cffm,cffc,'z',1,lpp_d2vz,curv(3))
+        !!! Compute the correction coefficient using trilinear interpolation on lookup data array 
+        !!! Note: Reynolds number calculated at previous substep is being used to avoid complicated implicit calculation
+        call CalcSlipCorrectionCoefficient(p,'x',coef(1))
+        call CalcSlipCorrectionCoefficient(p,'y',coef(2))
+        call CalcSlipCorrectionCoefficient(p,'z',coef(3))
+        !!! Compute the slip correction using coefficients and curvatures
+        slip(1) = slip(1) + coef(1)*curv(1)
+        slip(2) = slip(2) + coef(2)*curv(2)
+        slip(3) = slip(3) + coef(3)*curv(3)
+    end if
     !! Subtract fluid velocity from particle velocity to get slip velocity
-    slip(1) = p%lpp_vel(1)! - slip(1)
-    slip(2) = p%lpp_vel(2)! - slip(2)
-    slip(3) = p%lpp_vel(3)! - slip(3)
+    slip(1) = p%lpp_vel(1) - slip(1)
+    slip(2) = p%lpp_vel(2) - slip(2)
+    slip(3) = p%lpp_vel(3) - slip(3)
 
     ! Compute particle volume
     pvol = pi*(p%lpp_dia**3.0)/6.0
@@ -216,6 +212,7 @@ subroutine AddParticleAccelerationDrag(p,f)
     p%acc_now(2) = p%acc_now(2) - e2l_mult*forc(2)/(p%lpp_den*pvol)
     p%acc_now(3) = p%acc_now(3) - e2l_mult*forc(3)/(p%lpp_den*pvol)
 
+    ! Compile force result
     do i = 1,3
         f(1,i) = cffm(i)
         f(2,i) = cffc(i)
@@ -236,24 +233,20 @@ subroutine UpdateParticleVelocity(p)
     type(particle_data), intent(inout)  :: p
     integer                             :: i
 
-    ! if (p%lpp_lft.gt.0.0) then
-    !     ! Particle already exists in the domain
-    !     ! Update velocity at next sub step using RK-3 time-stepping scheme
-    !     do i = 1,3
-    !         p%lpp_vel(i) = p%lpp_vel(i) + dt*(ga*p%acc_now(i) + ro*p%acc_old(i))
-    !     end do
-    ! else
-    !     ! Particle has been freshly spawned
-    !     ! Update velocity at next sub step using truncated RK-3 (Euler) time-stepping scheme
-    !     do i = 1,3
-    !         p%lpp_vel(i) = p%lpp_vel(i) + (p%lpp_lft + al*dt)*p%acc_now(i)
-    !         p%acc_old(i) = p%acc_now(i)
-    !     end do
-    ! end if
-
-    do i = 1,3
-        p%lpp_vel(i) = p%lpp_vel(i) + dt*(ga*p%acc_now(i) + ro*p%acc_old(i))
-    end do
+    if (p%lpp_lft.gt.0.0) then
+        ! Particle already exists in the domain
+        ! Update velocity at next sub step using RK-3 time-stepping scheme
+        do i = 1,3
+            p%lpp_vel(i) = p%lpp_vel(i) + dt*(ga*p%acc_now(i) + ro*p%acc_old(i))
+        end do
+    else
+        ! Particle has been freshly spawned
+        ! Update velocity at next sub step using truncated RK-3 (Euler) time-stepping scheme
+        do i = 1,3
+            p%lpp_vel(i) = p%lpp_vel(i) + (p%lpp_lft + al*dt)*p%acc_now(i)
+            p%acc_old(i) = p%acc_now(i)
+        end do
+    end if
 
     return
 
@@ -269,23 +262,19 @@ subroutine UpdateParticlePosition(p)
     type(particle_data), intent(inout)  :: p
     integer                             :: i
 
-    ! if (p%lpp_lft.gt.0.0) then
-    !     ! Particle already exists in the domain
-    !     ! Update position at next sub-step assuming constant acceleration during sub-step (equivalent to Crank-Nicolson)
-    !     do i = 1,3
-    !         p%lpp_pos(i) = p%lpp_pos(i) + al*dt*(p%lpp_vel(i) - 0.5*dt*(ga*p%acc_now(i) + ro*p%acc_old(i)))
-    !     end do
-    ! else
-    !     ! Particle has been freshly spawned
-    !     ! Update velocity at next sub step using truncated RK-3 (Euler) time-stepping scheme
-    !     do i = 1,3
-    !         p%lpp_pos(i) = p%lpp_pos(i) + (p%lpp_lft + al*dt)*(p%lpp_vel(i) - 0.5*dt*(ga*p%acc_now(i) + ro*p%acc_old(i)))
-    !     end do
-    ! end if
-
-    do i = 1,3
-        p%lpp_pos(i) = p%lpp_pos(i) + al*dt*(p%lpp_vel(i) - 0.5*dt*(ga*p%acc_now(i) + ro*p%acc_old(i)))
-    end do
+    if (p%lpp_lft.gt.0.0) then
+        ! Particle already exists in the domain
+        ! Update position at next sub-step assuming constant acceleration during sub-step (equivalent to Crank-Nicolson)
+        do i = 1,3
+            p%lpp_pos(i) = p%lpp_pos(i) + al*dt*(p%lpp_vel(i) - 0.5*dt*(ga*p%acc_now(i) + ro*p%acc_old(i)))
+        end do
+    else
+        ! Particle has been freshly spawned
+        ! Update velocity at next sub step using truncated RK-3 (Euler) time-stepping scheme
+        do i = 1,3
+            p%lpp_pos(i) = p%lpp_pos(i) + (p%lpp_lft + al*dt)*(p%lpp_vel(i) - 0.5*dt*(ga*p%acc_now(i) + ro*p%acc_old(i)))
+        end do
+    end if
 
     return
 
@@ -447,6 +436,7 @@ subroutine ApplyParticleDragForce(p,f,t)
     ! Ratio (fraction) of time spent by particle in domain to substep interval
     frac = t*l2e_mult/(al*dt)
 
+    ! Decompile interpolation coefficients and force
     do i = 1,3
         cffm(i) = f(1,i)
         cffc(i) = f(2,i)

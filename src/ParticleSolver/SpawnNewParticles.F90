b1bc49918b65c036d620359d0575cb37b8d4bb9d
Yerragolam <guru.yerragolam@cpp.canon>
2026-01-17 10:48:00 +0100

    Attempt #1
    Major changes to fix the continuation issue including asynchronous spawning

--- src/ParticleSolver/SpawnNewParticles.F90
+++ src/ParticleSolver/SpawnNewParticles.F90
@@ -9,125 +9,96 @@
 
 subroutine SpawnNewParticles
 
-    use mpih
-    use param, only: al,dt,time,pi,rey
-    use local_arrays, only: vx,vy,vz
+    use param, only: al,dt,time,rey
     use lagrangian_point_particle
 
     implicit none
 
-    logical             :: spwn
-    integer             :: nsrc,ncnt,nlpp,indx
-    real                :: tsta,tend
-    real                :: cffc(3)
-    real                :: cffm(3)
-    real                :: slvx,slvy,slvz,slip
+    integer :: spwn
+    real    :: nprv,nnxt
+    real    :: nsta,nend
+    real    :: tsub
 
-    ! Set the tolerance interval for triggering spawning of particles
-    tsta = time - al*dt - lpp_stol
-    tend = time - al*dt + lpp_stol
+    ! Find the time just before the beginning of substep
+    tsub = time - al*dt
+
+    ! Initialize source spawn counts to zero
+    src_spwn(:) = 0
 
     ! Count the total number of spawned particles to be added to main particle list
-    ncnt = 0
     do nsrc = 1,src_size
-        !! Initialize boolean spawn as true
-        spwn = .true.
-        !! Check if it's before the source end time
-        spwn = spwn.and.(tsta.le.src_list(nsrc)%src_end)
-        !! Check if the next spawn event is within tolerance of the current time
-        spwn = spwn.and.(src_list(nsrc)%src_sta.ge.tsta)
-        spwn = spwn.and.(src_list(nsrc)%src_sta.le.tend)
-        !! Check if the frequency is a non-zero positive value
-        spwn = spwn.and.(src_list(nsrc)%src_frq.gt.0.0)
-        !! If spawn conditions are met, increment spawn count
-        if (spwn) ncnt = ncnt + 1
+        !! Check for non-zero frequency
+        if (src_list(nsrc)%src_frq.gt.0.0) then
+            spwn = ceiling(tsub*src_list(nsrc)%src_frq)             ! Integer number of spawn event triggers already from source 
+            nprv = tsub*src_list(nsrc)%src_frq                      ! Fractional number of spawn event triggers before beginning of substep
+            nnxt = time*src_list(nsrc)%src_frq                      ! Fractional number of spawn event triggers until end of substep
+            nsta = src_list(nsrc)%src_sta*src_list(nsrc)%src_frq    ! Fractional number of spawn event triggers from source start time
+            nend = src_list(nsrc)%src_end*src_list(nsrc)%src_frq    ! Fractional number of spawn event triggers until source end time
+            do while (((real(spwn).ge.nprv).and.(real(spwn).lt.nnxt)).and.((real(spwn).ge.nsta).and.(real(spwn).le.nend)))
+                src_spwn(nsrc) = src_spwn(nsrc) + 1
+                spwn = spwn + 1
+            end do
+        end if
     end do
 
     ! Update the total spawned particle count
-    lpp_spwn = lpp_spwn + ncnt
+    lpp_spwn = lpp_spwn + sum(src_spwn)
 
     ! Find index position at which spawned particles must be added
     indx = lpp_actv
 
-    ! Calcualte total size needed to append to the current particle list
-    ncnt = ncnt + indx
+    ! Update the total active particle count
+    lpp_actv = lpp_actv + sum(src_spwn)
+
     ! If needed, extend the particle list
-    call ExtendParticleListBuffer(ncnt+1)
+    call ExtendParticleListBuffer(lpp_actv)
 
     ! Loop over sources and initialize spawned particles
     do nsrc = 1,src_size
-
-        !! Initialize boolean spawn as true
-        spwn = .true.
-        !! Check if it's before the source end time
-        spwn = spwn.and.(tsta.le.src_list(nsrc)%src_end)
-        !! Check if the next spawn event is within tolerance of the current time
-        spwn = spwn.and.(src_list(nsrc)%src_sta.ge.tsta)
-        spwn = spwn.and.(src_list(nsrc)%src_sta.le.tend)
-        !! Check if the frequency is a non-zero positive value
-        spwn = spwn.and.(src_list(nsrc)%src_frq.gt.0.0)
-        !! If spawn conditions are met, increment spawn count
-        if (spwn) then
-
-            !!! Update the index at which particle is spawned
-            indx = indx + 1
-            !!! Copy the source index
-            lpp_list(indx)%src_idx    = src_list(nsrc)%src_idx
-            !!! Initialize the [xc,yc,zc] grid indices to the source grid indices
-            lpp_list(indx)%grc_idx(1) = src_list(nsrc)%grc_idx(1)
-            lpp_list(indx)%grc_idx(2) = src_list(nsrc)%grc_idx(2)
-            lpp_list(indx)%grc_idx(3) = src_list(nsrc)%grc_idx(3)
-            !!! Initialize the [xm,ym,zm] grid indices to the source grid indices
-            lpp_list(indx)%grm_idx(1) = src_list(nsrc)%grm_idx(1)
-            lpp_list(indx)%grm_idx(2) = src_list(nsrc)%grm_idx(2)
-            lpp_list(indx)%grm_idx(3) = src_list(nsrc)%grm_idx(3)
-            !!! Set the lifetime of particle at injection to zero 
-            lpp_list(indx)%lpp_lft    = 0.0
-            !!! Initialize particle diameter and density
-            lpp_list(indx)%lpp_dia    = src_list(nsrc)%src_dia
-            lpp_list(indx)%lpp_den    = src_list(nsrc)%src_den
-            !!! Initialize particle position to source position
-            lpp_list(indx)%lpp_pos(1) = src_list(nsrc)%src_pos(1)
-            lpp_list(indx)%lpp_pos(2) = src_list(nsrc)%src_pos(2) 
-            lpp_list(indx)%lpp_pos(3) = src_list(nsrc)%src_pos(3) 
-            !!! Initialize particle velocity to injection velocity
-            lpp_list(indx)%lpp_vel(1) = src_list(nsrc)%src_vel(1)
-            lpp_list(indx)%lpp_vel(2) = src_list(nsrc)%src_vel(2)
-            lpp_list(indx)%lpp_vel(3) = src_list(nsrc)%src_vel(3)
-            !!! Initialize old acceleration to zero
-            lpp_list(indx)%acc_old(1) = 0.0
-            lpp_list(indx)%acc_old(2) = 0.0
-            lpp_list(indx)%acc_old(3) = 0.0
-            !!! Initialize current acceleration to zero
-            lpp_list(indx)%acc_now(1) = 0.0
-            lpp_list(indx)%acc_now(2) = 0.0
-            lpp_list(indx)%acc_now(3) = 0.0
-            !!! Initialize the particle reynolds number using the slip (no slip correction needed at initialization)
-            !!!! Compute interpolation coefficients
-            call CalcTrilinearInterpolationCoefficients(lpp_list(indx),cffm,cffc)
-            slvx = 0.0
-            slvy = 0.0
-            slvz = 0.0
-            !!!! Apply trilinear interpolation with previously calculated coefficients to get fluid velocity
-            call ApplyTrilinearInterpolation(lpp_list(indx),cffm,cffc,'x',1,vx,slvx)
-            call ApplyTrilinearInterpolation(lpp_list(indx),cffm,cffc,'y',1,vy,slvy)
-            call ApplyTrilinearInterpolation(lpp_list(indx),cffm,cffc,'z',1,vz,slvz)
-            !!!! Subtract fluid velocity from source velocity to get slip velocity
-            slvx = src_list(nsrc)%src_vel(1) - slvx
-            slvy = src_list(nsrc)%src_vel(2) - slvy
-            slvz = src_list(nsrc)%src_vel(3) - slvz
-            slip = sqrt((slvx**2.0) + (slvy**2.0) + (slvz**2.0))
-            lpp_list(indx)%lpp_rey    = lpp_list(indx)%lpp_dia*rey*slip
-            !!! Frequency is a non-zero positive number, so update the next spawn time instant
-            src_list(nsrc)%src_sta = src_list(nsrc)%src_sta + (1.0/src_list(nsrc)%src_frq)
-
+        !! Check for non-zero frequency and non-zero spawned particles
+        if ((src_list(nsrc)%src_frq.gt.0.0).and.(src_spwn(nsrc).gt.0)) then
+            !!! Get the first spawn trigger instance after the start of substep 
+            spwn = ceiling(tsub*src_list(nsrc)%src_frq)
+            do nlpp = 1,src_spwn(nsrc)
+                !!!! Update the index at which particle is spawned
+                indx = indx + 1
+                !!!! Copy the source index
+                lpp_list(indx)%src_idx    = src_list(nsrc)%src_idx
+                !!!! Initialize the [xc,yc,zc] grid indices to the source grid indices
+                lpp_list(indx)%grc_idx(1) = src_list(nsrc)%grc_idx(1)
+                lpp_list(indx)%grc_idx(2) = src_list(nsrc)%grc_idx(2)
+                lpp_list(indx)%grc_idx(3) = src_list(nsrc)%grc_idx(3)
+                !!!! Initialize the [xm,ym,zm] grid indices to the source grid indices
+                lpp_list(indx)%grm_idx(1) = src_list(nsrc)%grm_idx(1)
+                lpp_list(indx)%grm_idx(2) = src_list(nsrc)%grm_idx(2)
+                lpp_list(indx)%grm_idx(3) = src_list(nsrc)%grm_idx(3)
+                !!!! Set the lifetime of particle at injection to a suitable negative value 
+                !!!! Important to keep it negative as this helps ParticleMarchSubstep to detect the spawn and use truncated RK3 
+                lpp_list(indx)%lpp_lft    = tsub - spwn/src_list(nsrc)%src_frq
+                !!!! Initialize particle diameter
+                lpp_list(indx)%lpp_dia    = src_list(nsrc)%src_dia
+                !!!! Initialize particle density
+                lpp_list(indx)%lpp_den    = src_list(nsrc)%src_den
+                !!!! Initialize particle Reynolds number (will be overwritten during ParticleMarchSubstep)
+                lpp_list(indx)%lpp_den    = src_list(nsrc)%src_dia*norm2(src_list(nsrc)%src_vel)*rey
+                !!!! Initialize particle position to source position
+                lpp_list(indx)%lpp_pos(1) = src_list(nsrc)%src_pos(1)
+                lpp_list(indx)%lpp_pos(2) = src_list(nsrc)%src_pos(2) 
+                lpp_list(indx)%lpp_pos(3) = src_list(nsrc)%src_pos(3) 
+                !!!! Initialize particle velocity to injection velocity
+                lpp_list(indx)%lpp_vel(1) = src_list(nsrc)%src_vel(1)
+                lpp_list(indx)%lpp_vel(2) = src_list(nsrc)%src_vel(2)
+                lpp_list(indx)%lpp_vel(3) = src_list(nsrc)%src_vel(3)
+                !!!! Initialize old acceleration to zero (will be overwritten during ParticleMarchSubstep)
+                lpp_list(indx)%acc_old(1) = 0.0
+                lpp_list(indx)%acc_old(2) = 0.0
+                lpp_list(indx)%acc_old(3) = 0.0
+                !!!! Initialize current acceleration to zero (will be overwritten during ParticleMarchSubstep)
+                lpp_list(indx)%acc_now(1) = 0.0
+                lpp_list(indx)%acc_now(2) = 0.0
+                lpp_list(indx)%acc_now(3) = 0.0
+            end do
         end if
-
     end do
-
-    ! Update the number of active particles to include the newly spawned particles
-    lpp_actv = ncnt
-
-    return
 
 end subroutine SpawnNewParticles
